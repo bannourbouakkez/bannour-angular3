e8e35a94f7e09beadea4f2e353ca3b8d8e19e9c9
Bannour <bouakkezbannour@gmail.com>
2021-04-09 01:35:05 +0200

    T

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'http://localhost:8000/api'
+  //apiUrl:'http://localhost:8000/api'
+  apiUrl:'https://bannour-laravel.herokuapp.com/api'
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl:'http://localhost:8000/api'
+  //apiUrl:'http://localhost:8000/api'
+  apiUrl:'https://bannour-laravel.herokuapp.com/api'
+  
 };
 
 /*
